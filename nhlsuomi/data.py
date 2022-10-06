fb6ceb94111240bfe14cfa65cf53656c85ce67c2
Miikka Kulmala <milzer@users.noreply.github.com>
2022-10-06 19:18:42 +0300

    Game dataclass

--- nhlsuomi/data.py
+++ nhlsuomi/data.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from functools import total_ordering
-from typing import Any, Tuple
+from typing import Any, List, Optional, Tuple
 
 
 @total_ordering
@@ -38,7 +38,7 @@ class Player:
         else:
             return NotImplemented
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         if isinstance(other, Player):
             return self._value < other._value
         else:
@@ -55,4 +55,43 @@ class Player:
             self.shots,
             self.hits,
             self.pim
+        )
+
+
+@total_ordering
+@dataclass(eq=False)
+class Game:
+    home_team: str
+    home_score: int
+    away_team: str
+    away_score: int
+    type: str
+    gamecenter_id: str
+    recap_url: Optional[str] = None
+    players: List[Player] = field(default_factory=list)
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, Game):
+            return self._value == other._value
+        else:
+            return NotImplemented
+
+    def __lt__(self, other) -> bool:
+        if isinstance(other, Game):
+            return self._value < other._value
+        else:
+            return NotImplemented
+
+    @property
+    def _value(self) -> Tuple[int, int, int]:
+        points = [
+            (p.g, p.a)
+            for p in self.players
+            if (p.g + p.a) > 0
+        ]
+
+        return (
+            len(points),  # number of scorers
+            sum(g + a for g, a in points),  # total points
+            len(self.players)
         )
---
+++ tests/test_data_game.py
@@ -0,0 +1,39 @@
+import unittest
+
+from nhlsuomi.data import Game, Player
+
+game1 = Game('COL',  5, 'TBL',  0, 'regular', '1', None, [
+    Player('a', 0, 0, 0, 0, 0, 0, 0),
+    Player('b', 0, 0, 0, 0, 0, 0, 0),
+    Player('c', 0, 0, 0, 0, 0, 0, 0),
+    Player('d', 0, 0, 0, 0, 0, 0, 0)
+])
+
+game2 = Game('COL',  5, 'TBL',  0, 'regular', '2', None, [
+    Player('e', 1, 0, 0, 0, 0, 0, 0),
+    Player('f', 0, 1, 0, 0, 0, 0, 0),
+    Player('g', 0, 0, 0, 0, 0, 0, 0)
+])
+
+game3 = Game('COL',  5, 'TBL',  0, 'regular', '3', None, [
+    Player('h', 3, 0, 0, 0, 0, 0, 0)
+])
+
+game4 = Game('COL',  5, 'TBL',  0, 'regular', '4', None, [
+    Player('i', 0, 3, 0, 0, 0, 0, 0)
+])
+
+
+class TestGameData(unittest.TestCase):
+    def test_eq(self):
+        self.assertTrue(game3 == game4)
+
+    def test_lt(self):
+        self.assertLess(game1, game2)
+        self.assertLess(game1, game3)
+        self.assertLess(game3, game2)
+
+    def test_ordering(self):
+        games = [game1, game2, game3]
+        sorted_ids = [g.gamecenter_id for g in sorted(games, reverse=True)]
+        self.assertListEqual(sorted_ids, ['2', '3', '1'])
