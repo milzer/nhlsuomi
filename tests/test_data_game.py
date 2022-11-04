646294941da8c2ac1d411f155624940b3453168e
Miikka Kulmala <milzer@users.noreply.github.com>
2022-11-04 14:46:00 +0200

    Fix Game tests

--- tests/test_data_game.py
+++ tests/test_data_game.py
@@ -2,24 +2,24 @@ import unittest
 
 from nhlsuomi.data import Game, Player
 
-game1 = Game('COL',  5, 'TBL',  0, True, 1, [
+game1 = Game('COL',  5, 'TBL',  0, True, 1, None, [
     Player('a', 'a', 0, 0, 0, 0, 0, 0, 0),
     Player('b', 'b', 0, 0, 0, 0, 0, 0, 0),
     Player('c', 'c', 0, 0, 0, 0, 0, 0, 0),
     Player('d', 'd', 0, 0, 0, 0, 0, 0, 0)
 ])
 
-game2 = Game('COL',  5, 'TBL',  0, True, 2, [
+game2 = Game('COL',  5, 'TBL',  0, True, 2, None, [
     Player('e', 'e', 1, 0, 0, 0, 0, 0, 0),
     Player('f', 'f', 0, 1, 0, 0, 0, 0, 0),
     Player('g', 'g', 0, 0, 0, 0, 0, 0, 0)
 ])
 
-game3 = Game('COL',  5, 'TBL',  0, True, 3, [
+game3 = Game('COL',  5, 'TBL',  0, True, 3, None, [
     Player('h', 'h', 3, 0, 0, 0, 0, 0, 0)
 ])
 
-game4 = Game('COL',  5, 'TBL',  0, False, 4, [
+game4 = Game('COL',  5, 'TBL',  0, False, 4, None, [
     Player('i', 'i', 1, 0, 0, 0, 0, 0, 0),
     Player('j', 'j', 1, 0, 0, 0, 0, 0, 0),
     Player('k', 'k', 1, 0, 0, 0, 0, 0, 0),
