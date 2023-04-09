8b0080ade905c3a00673da792518c46bd792467b
Miikka Kulmala <milzer@users.noreply.github.com>
2023-04-09 14:27:14 +0300

    Retry failed url requests

--- nhlsuomi/NHL/API.py
+++ nhlsuomi/NHL/API.py
@@ -1,7 +1,9 @@
 import json
+import time
 import urllib.request
 from datetime import date, timedelta
 from typing import Mapping, Optional
+from urllib.error import URLError
 from urllib.parse import urlencode
 
 from nhlsuomi.logging import logger
@@ -9,12 +11,22 @@ from nhlsuomi.logging import logger
 BASE_URL = 'https://statsapi.web.nhl.com/api/v1'
 
 
-def _fetch_json(url: str) -> Mapping:
-    logger.info(f'Fetch {url}')
-    with urllib.request.urlopen(url) as res:
-        data = res.read()
-        encoding = res.info().get_content_charset('utf-8')
-        return json.loads(data.decode(encoding))
+def _fetch_json(url: str, *, retries: int = 3) -> Mapping:
+    for retry in range(retries + 1):
+        try:
+            action = 'Retry' if retry else 'Fetch'
+            logger.info(f'{action} {url}')
+
+            with urllib.request.urlopen(url) as res:
+                data = res.read()
+                encoding = res.info().get_content_charset('utf-8')
+                return json.loads(data.decode(encoding))
+
+        except URLError as e:
+            logger.warning(f'Fetch failed: {e}')
+            time.sleep(10)
+
+    raise RuntimeError('Too many retries')
 
 
 def get_schedule(date: Optional[date] = None, days: int = 1) -> Mapping:
